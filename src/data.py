aec8e2d7645439b233abbd6d7a2d5e9f3532399d
Erik Saule <esaule@uncc.edu>
2021-08-02 20:53:20 -0400

    stripping unnecessary imports

--- src/data.py
+++ src/data.py
@@ -2,9 +2,6 @@ import requests
 import json
 
 
-
-
-
 # takes an id of a collection and returns a list of all the materials contained by that collection (not recursive)
 def all_materials_in_collection(id):
     url = "https://cs-materials-api.herokuapp.com/data/material/meta?id=" + str(id)
